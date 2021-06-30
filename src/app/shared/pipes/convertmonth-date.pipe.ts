93ece171e73e815b512a6f6161b1225b0ef34881
elleblue0023 <lovanirinamurelle@gmail.com>
2021-06-30 20:15:36 +0300

    continue login + affichage liste matchs

---
+++ src/app/shared/pipes/convertmonth-date.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convertmonthDate'
+})
+export class ConvertmonthDatePipe implements PipeTransform {
+
+  transform(value: number): string {
+    let monthListShort: { [key: string]: string } = {
+      "01": "JANV",
+      "02": "FEV",
+      "03": "MARS",
+      "04": "AVR",
+      "05": "MAI",
+      "06": "JUI",
+      "07": "JUIL",
+      "08": "AOU",
+      "09": "SEP",
+      "10": "OCT",
+      "11": "NOV",
+      "12": "DEC"
+    }
+    return monthListShort[value];
+  }
+
+}

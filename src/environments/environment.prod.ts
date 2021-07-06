cd39440cf4c88cb85775ed5e69cb2987ce927057
elleblue0023 <lovanirinamurelle@gmail.com>
2021-07-06 11:34:38 +0300

    build prod script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'https://pari-node.herokuapp.com/api',
+  photo_endpoint : 'https://pari-node.herokuapp.com/',
+  node_endpoint: 'https://pari-node.herokuapp.com/api',
+  grails_endpoint: 'https://mbdsp7-grails-app.herokuapp.com/'
 };

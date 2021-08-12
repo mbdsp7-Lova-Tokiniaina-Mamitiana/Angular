c96d2b0f52c950e6ec45ccabd7c22764bc55b635
elleblue0023 <lovanirinamurelle@gmail.com>
2021-08-12 15:47:33 +0300

    update grails endpoint api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   endpoint: 'https://pari-node.herokuapp.com/api',
   photo_endpoint : 'https://pari-node.herokuapp.com/',
   node_endpoint: 'https://pari-node.herokuapp.com/api',
-  grails_endpoint: 'https://mbdsp7-grails-app.herokuapp.com/'
+  grails_endpoint: 'https://mbdsp7-grails-app.herokuapp.com/api',
 };
